f92153e17e6b948d2a3b240a207c245ec4938e8f
kristjgr <kristinejun@hotmail.com>
2021-04-22 11:25:08 +0200

    new neural network solution

--- Assignment2/task1.py
+++ Assignment2/task1.py
@@ -4,157 +4,172 @@ import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
 from sklearn.neighbors import KNeighborsRegressor
 from sklearn.svm import SVR
-from sklearn import metrics
-from sklearn.neural_network import MLPRegressor
-
-from sklearn.preprocessing import MinMaxScaler
-
+from sklearn.metrics import mean_squared_error
+from keras.layers import Input, Dense
+from keras.models import Model
 
 
-#TODO scale data?
-
-
-def RMSE(x, y):
-   return np.sqrt(np.mean((x-y)**2))
-
-#Training data
-df=pd.read_csv('TrainData.csv', sep=',')
-
-windspeed = df["WS10"].to_numpy().reshape((-1,1))
+# ---------------- Reading from file ----------------
+# Training data
+df = pd.read_csv('TrainData.csv', sep=',')
+windspeed = df["WS10"].to_numpy().reshape((-1, 1))
 power = df["POWER"].to_numpy()
-
 u = df["U10"].to_numpy()
 v = df["V10"].to_numpy()
+direction = np.mod(180 + np.rad2deg(np.arctan2(v, u)), 360)
 
-direction = np.mod(180+np.rad2deg(np.arctan2(v,u)), 360)
-
-#Input data
+# Input data
 df = pd.read_csv("WeatherForecastInput.csv", sep=',')
-ws = df["WS10"].to_numpy().reshape((-1,1))
-
+ws = df["WS10"].to_numpy().reshape((-1, 1))
 u_input = df["U10"].to_numpy()
 v_input = df["V10"].to_numpy()
+direction_input = np.mod(180 + np.rad2deg(np.arctan2(v_input, u_input)), 360).reshape((-1, 1))
+x_input = np.concatenate((ws, direction_input), axis=1)
 
-direction_input = np.mod(180+np.rad2deg(np.arctan2(v_input,u_input)), 360).reshape((-1,1))
-
-x_input = np.concatenate((ws,direction_input),axis=1)
-
-#solution - actual power
-df = pd.read_csv("Solution.csv", sep = ',')
+# Solution - actual power
+df = pd.read_csv("Solution.csv", sep=',')
 time = df["TIMESTAMP"].to_numpy()
 actualPower = df["POWER"].to_numpy()
 
 
+def RMSE(x, y):
+    return np.sqrt(np.mean((x - y) ** 2))
 
 
+def plotResults(actualPower, predictedPower, model):
+    fig, ax = plt.subplots(figsize=(9, 5))
+    ax.set_xticklabels([])
+    plt.plot(actualPower, color='lightseagreen', label="actual power")
+    plt.plot(predictedPower, color='darkorange', label="predicted power")
+    plt.legend()
+    # plt.legend( bbox_to_anchor=(1.05, 1), loc='upper center')
+
+    plt.title("True vs. predicted wind power, " + model)
+    plt.xlabel("Time")
+    plt.ylabel("Power")
+    plt.show()
+
+# ---------------- For task 1 ----------------
 def linearRegression(windspeed, power, forecastInput, actualPower):
     model = LinearRegression()
     model.fit(windspeed, power)
- 
 
-    #using the model to predict new data
-
+    # Predicting power with new data
     predictedPower = model.predict(forecastInput)
-   
+
     print("RMSE linear regression ", RMSE(actualPower, predictedPower))
-    #print("RMSE2  ", np.sqrt(metrics.mean_squared_error(actualPower, predictedPower)))
 
-    plt.scatter(forecastInput, actualPower, s = 1.0)
-    plt.plot(forecastInput, predictedPower, color = "r")
-    plt.show()
-
+    plotResults(actualPower, predictedPower, "linear regression")
 
 
 def knn(windspeed, power, forecastInput, actualPower):
-   
-    for k in range(190,191):
+    for k in range(190, 191):
         model = KNeighborsRegressor(n_neighbors=k)
         model.fit(windspeed, power)
 
         predictedPower = model.predict(forecastInput)
 
-        plt.scatter(forecastInput, actualPower, c = "r", s = 1.5)
-        plt.scatter(forecastInput, predictedPower, c = "b", s = 1.5)
+        plt.scatter(forecastInput, actualPower, c="r", s=1.5)
+        plt.scatter(forecastInput, predictedPower, c="b", s=1.5)
         plt.show()
 
-        print("RMSE knn k = %d : %f" %  (k, RMSE(predictedPower, actualPower)))
+        print("RMSE knn k = %d : %f" % (k, RMSE(predictedPower, actualPower)))
+
+    # plotResults(actualPower, predictedPower, "knn")
+
 
 def svr(windspeed, power, forecastInput, actualPower):
     model = SVR()
     model.fit(windspeed, power)
 
+    # Predicting power with new data
     predictedPower = model.predict(forecastInput)
-    plt.scatter(forecastInput, actualPower, c = "r", s = 1.0)
-    
-    plt.scatter(forecastInput, predictedPower, c = "b", s = 1.0)
-    plt.show()
 
     print("RMSE svr", RMSE(actualPower, predictedPower))
 
+    plotResults(actualPower, predictedPower, "svr")
 
 
 def neuralNetwork(windspeed, power, forecastInput, actualPower):
-    scaler = MinMaxScaler()
-    windspeed = scaler.fit_transform(windspeed) #scaling the data between 0 and 1
+    # This returns a tensor. Since the input only has one column
+    inputs = Input(shape=(1,))
 
-    nn = MLPRegressor(hidden_layer_sizes=(100,100), max_iter=1000)
+    # Layers and number of neurons in hidden layers
+    x = Dense(32, activation='relu')(inputs)
+    x = Dense(64, activation='relu')(x)
+    x = Dense(100, activation='relu')(x)
+    predictions = Dense(1)(x)
 
-    nn.fit(windspeed, power)
+    # The model that includes the input layer and three Dense layers
+    model = Model(inputs=inputs, outputs=predictions)
+    model.compile(optimizer='rmsprop',
+                  loss='mse',
+                  metrics=['mse'])
 
-    forecastInput = scaler.fit_transform(forecastInput)
-    predictedPower = nn.predict(forecastInput)
+    model.fit(windspeed, power, epochs=500, batch_size=100, verbose=1, validation_split=0.2)  # starts training
 
-    plt.scatter(forecastInput, actualPower, c = "r", s = 1.0)
-    plt.plot(forecastInput, predictedPower, c = "b")
+    # Predicting power with new data
+    predictedPower = model.predict(forecastInput)
+
+    actualPower = actualPower.reshape((-1, 1))
+    print("RMSE Neural networks", RMSE(actualPower, predictedPower))
+
+    plt.scatter(forecastInput, actualPower, c="r", s=1.0)
+    plt.scatter(forecastInput, predictedPower, c="b")
+    plt.title("neural network")
+
     plt.show()
 
-    print("RMSE neural network", RMSE(actualPower, predictedPower))
-    
-
-#linearRegression(windspeed, power, ws, actualPower)
-#knn(windspeed, power, ws, actualPower)
-#svr(windspeed, power, ws, actualPower)
-#neuralNetwork(windspeed, power, ws, actualPower)
+    plotResults(actualPower, predictedPower, "neural network")
 
 
-def multiple_lin_reg(windspeed, direction, power, forecastInput, actualPower):
-    direction = direction.reshape((-1,1))
+# linearRegression(windspeed, power, ws, actualPower)
+# knn(windspeed, power, ws, actualPower)
+# svr(windspeed, power, ws, actualPower)
+neuralNetwork(windspeed, power, ws, actualPower)
+
+
+
+
+# ---------------- For task 2 ----------------
+def multipleLinReg(windspeed, direction, power, forecastInput, actualPower):
+    direction = direction.reshape((-1, 1))
 
     x = np.concatenate((windspeed, direction), axis=1)
-    
+
     model = LinearRegression()
-    model.fit(x,power)
+    model.fit(x, power)
 
     predictedPower = model.predict(forecastInput)
 
     print("RMSE multiple linear regression", RMSE(actualPower, predictedPower))
-    
-
-multiple_lin_reg(windspeed, direction, power, x_input, actualPower)
 
 
-def time_series_lin_reg(powers, windowsize, time, actualPower):
+# multiple_lin_reg(windspeed, direction, power, x_input, actualPower)
 
-    #powers = np.array(range(10))
 
+
+
+
+# ---------------- For task 3 ----------------
+def timeseriesLinReg(powers, windowsize, time, actualPower):
+    # powers = np.array(range(10))
 
     rows = len(powers) - windowsize
     x = np.zeros((rows, windowsize))
     y = np.zeros(rows)
-    
+
     for i in range(rows):
         for j in range(windowsize):
-            x[i][j] = powers[i+j] 
+            x[i][j] = powers[i + j]
 
-        y[i] = powers[i+windowsize]
+        y[i] = powers[i + windowsize]
 
     model = LinearRegression()
-    model.fit(x,y)
+    model.fit(x, y)
 
- 
+    predictedPower = model.predict(actualPower[:-1].reshape(-1, 1))
 
-    predictedPower = model.predict(actualPower[:-1].reshape(-1,1))
-    
     powerSolution = actualPower[1:]
 
     print("RMSE time series linear regression ", RMSE(powerSolution, predictedPower))
@@ -164,24 +179,7 @@ def time_series_lin_reg(powers, windowsize, time, actualPower):
     plt.plot(time, predictedPower)
     plt.show()
 
+    # predicted = model.predict(powers[-1].reshape(1,-1))
+    # nextPredicted = model.predict(predicted.reshape(1,-1))
 
-
-
-
-
-    #predicted = model.predict(powers[-1].reshape(1,-1))
-    #nextPredicted = model.predict(predicted.reshape(1,-1))
-
-
-
-
-
-
-
-    
-
-time_series_lin_reg(power, 1, time, actualPower)
-
-
-
-
+# time_series_lin_reg(power, 1, time, actualPower)
