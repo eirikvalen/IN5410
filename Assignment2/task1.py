778edbda69bda47d2f6cc03a052e34e38e05d271
kristjgr <kristinejun@hotmail.com>
2021-05-07 12:10:01 +0200

    finished task3

--- Assignment2/task1.py
+++ Assignment2/task1.py
@@ -10,26 +10,24 @@ from keras.models import Model
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Dropout, LSTM
 
-
-
-# ---------------- Reading from file ----------------
+# ---------------- Reading from files ----------------
 # Training data
 df = pd.read_csv('TrainData.csv', sep=',')
-windspeed = df["WS10"].to_numpy().reshape((-1, 1))
-power = df["POWER"].to_numpy()
+windspeed_training = df["WS10"].to_numpy().reshape((-1, 1))
+power_training = df["POWER"].to_numpy()
 u = df["U10"].to_numpy()
 v = df["V10"].to_numpy()
-direction = np.mod(180 + np.rad2deg(np.arctan2(v, u)), 360)
+wind_direction = np.mod(180 + np.rad2deg(np.arctan2(v, u)), 360)
 
 # Input data
 df = pd.read_csv("WeatherForecastInput.csv", sep=',')
-ws = df["WS10"].to_numpy().reshape((-1, 1))
+windspeed_input = df["WS10"].to_numpy().reshape((-1, 1))
 u_input = df["U10"].to_numpy()
 v_input = df["V10"].to_numpy()
 direction_input = np.mod(180 + np.rad2deg(np.arctan2(v_input, u_input)), 360).reshape((-1, 1))
-x_input = np.concatenate((ws, direction_input), axis=1)
+windspeed_and_direction_input = np.concatenate((windspeed_input, direction_input), axis=1)
 
-# Solution - actual power
+# Solution (actual power)
 df = pd.read_csv("Solution.csv", sep=',')
 time = df["TIMESTAMP"].to_numpy()
 actualPower = df["POWER"].to_numpy()
@@ -52,70 +50,74 @@ def plotResults(actualPower, predictedPower, model):
     plt.ylabel("Power")
     plt.show()
 
-def plotResults3Inputs(actualPower, predictedpower1, predictedPower):
+
+def plotResults3Inputs(actualPower, predictedpower1, predictedPower, label1, label2, label3, title):
     fig, ax = plt.subplots(figsize=(9, 5))
     ax.set_xticklabels([])
-    plt.plot(actualPower, color='gray', label="actual power")
-    plt.plot(predictedpower1, color='lightseagreen', label="linear regression")
-    plt.plot(predictedPower, color='darkorange', label="multiple linear regression")
+    plt.plot(actualPower, color='gray', label=label1)
+    plt.plot(predictedpower1, color='lightseagreen', label=label2)
+    plt.plot(predictedPower, color='darkorange', label=label3)
     plt.legend()
-    # plt.legend( bbox_to_anchor=(1.05, 1), loc='upper center')
 
-    plt.title("Simple, multiple and actual wind power")
+    plt.title(title)
     plt.xlabel("Time")
     plt.ylabel("Power")
     plt.show()
 
+
 def scatterPlot(forecastInput, actualPower, predictedPower, model):
-    plt.scatter(forecastInput, actualPower, c="r", s=1.5)
-    plt.scatter(forecastInput, predictedPower, c="b", s=1.5)
+    plt.scatter(forecastInput, actualPower, c="r", s=1.5, label="Actual")
+    plt.scatter(forecastInput, predictedPower, c="b", s=1.5, label="Predicted")
     plt.title("True vs. predicted wind power, " + model)
     plt.xlabel("Time")
     plt.ylabel("Power")
     plt.show()
 
 
-
 # ---------------- For task 1 ----------------
-def linearRegression(windspeed, power, forecastInput, actualPower):
+def linearRegression(windspeed, power, forecastInput, actualPower, showPlots):
     model = LinearRegression()
     model.fit(windspeed, power)
 
-    # Predicting power with new data
     predictedPower = model.predict(forecastInput)
 
-    print("RMSE linear regression ", RMSE(actualPower, predictedPower))
+    rmse = RMSE(actualPower, predictedPower)
 
-    # plotResults(actualPower, predictedPower, "linear regression")
+    if showPlots:
+        plotResults(actualPower, predictedPower, "linear regression")
 
-    return predictedPower
+    return predictedPower, rmse
 
 
-def kNearestNeighbor(windspeed, power, forecastInput, actualPower):
+def kNearestNeighbor(windspeed, power, forecastInput, actualPower, showPlots):
     for k in [20]:
         model = KNeighborsRegressor(n_neighbors=k)
         model.fit(windspeed, power)
 
         predictedPower = model.predict(forecastInput)
 
-        print("RMSE KNN k = %d : %f" % (k, RMSE(predictedPower, actualPower)))
+        rmse = RMSE(predictedPower, actualPower)
 
-        plotResults(actualPower, predictedPower, "KNN")
+        if showPlots:
+            plotResults(actualPower, predictedPower, "KNN")
+
+        return predictedPower, rmse
 
 
-def supportedVectorRegression(windspeed, power, forecastInput, actualPower):
+def supportedVectorRegression(windspeed, power, forecastInput, actualPower, showPlots):
     model = SVR()
     model.fit(windspeed, power)
 
-    # Predicting power with new data
     predictedPower = model.predict(forecastInput)
 
-    print("RMSE SVR", RMSE(actualPower, predictedPower))
+    rmse = RMSE(actualPower, predictedPower)
 
-    plotResults(actualPower, predictedPower, "SVR")
+    if showPlots:
+        plotResults(actualPower, predictedPower, "SVR")
 
+    return predictedPower, rmse
 
-def artificialNeuralNetworks(windspeed, power, forecastInput, actualPower):
+def artificialNeuralNetworks(windspeed, power, forecastInput, actualPower, showPlots):
     # This returns a tensor. Since the input only has one column
     inputs = Input(shape=(1,))
 
@@ -131,29 +133,39 @@ def artificialNeuralNetworks(windspeed, power, forecastInput, actualPower):
                   loss='mse',
                   metrics=['mse'])
 
-    model.fit(windspeed, power, epochs=500, batch_size=100, verbose=1, validation_split=0.2)  # starts training
+    model.fit(windspeed, power, epochs=100, batch_size=100, verbose=1, validation_split=0.2)
 
-    # Predicting power with new data
+    # Predicting power_training with new data
     predictedPower = model.predict(forecastInput)
 
     actualPower = actualPower.reshape((-1, 1))
 
-    print("RMSE Neural networks", RMSE(actualPower, predictedPower))
+    rmse = RMSE(actualPower, predictedPower)
 
-    # scatterPlot(forecastInput, actualPower, predictedPower, "Neural networks")
-    # plotResults(actualPower, predictedPower, "neural network")
+    if showPlots:
+        plotResults(actualPower, predictedPower, "neural network")
+
+    return predictedPower, rmse
 
 
-# linearRegression(windspeed, power, ws, actualPower)
-# kNearestNeighbor(windspeed, power, ws, actualPower)
-# supportedVectorRegression(windspeed, power, ws, actualPower)
-# artificialNeuralNetworks(windspeed, power, ws, actualPower)
+def runAllModelsTask1(showPlots):
+    linReg = linearRegression(windspeed_training, power_training, windspeed_input, actualPower, showPlots)
+    KNN = kNearestNeighbor(windspeed_training, power_training, windspeed_input, actualPower, showPlots)
+    SVR = supportedVectorRegression(windspeed_training, power_training, windspeed_input, actualPower, showPlots)
+    ANN = artificialNeuralNetworks(windspeed_training, power_training, windspeed_input, actualPower, showPlots)
 
+    print("RMSE timeseries LinReg:" + str(linReg[1]))
+    print("RMSE timeseries KNN:" + str(KNN[1]))
+    print("RMSE timeseries SVR:" + str(SVR[1]))
+    print("RMSE timeseries ANN:" + str(ANN[1]))
+
+
+# runAllModelsTask1(True)
 
 
 
 # ---------------- For task 2 ----------------
-def multipleLinReg(windspeed, direction, power, forecastInput, actualPower):
+def multipleLinReg(windspeed, direction, power, forecastInput, actualPower, showPlots):
     direction = direction.reshape((-1, 1))
 
     x = np.concatenate((windspeed, direction), axis=1)
@@ -163,23 +175,38 @@ def multipleLinReg(windspeed, direction, power, forecastInput, actualPower):
 
     predictedPower = model.predict(forecastInput)
 
-    print("RMSE multiple linear regression", RMSE(actualPower, predictedPower))
+    rmse = RMSE(actualPower, predictedPower)
 
-    # plotResults(actualPower, predictedPower, "multiple linear regression")
+    if showPlots:
+        plotResults(actualPower, predictedPower, "multiple linear regression")
 
-    return predictedPower
+    return predictedPower, rmse
 
 
+def runAllModelsTask2(showPlots):
+    simpleLinearRegression = linearRegression(windspeed_training, power_training, windspeed_input, actualPower, showPlots)
+    multipleLinearRegression = multipleLinReg(windspeed_training, wind_direction, power_training,
+                                              windspeed_and_direction_input,
+                                              actualPower,
+                                              showPlots)
+    if showPlots:
+        plotResults3Inputs(actualPower,
+                           simpleLinearRegression[0],
+                           multipleLinearRegression[0],
+                           "actual power",
+                           "linear regression",
+                           "multiple regression",
+                           "Simple, multiple and actual wind power")
 
-# plotResults3Inputs(actualPower,
-#              linearRegression(windspeed, power, ws, actualPower),
-#              multipleLinReg(windspeed, direction, power, x_input, actualPower))
+    print("RMSE simple linear regression: " + str(simpleLinearRegression[1]))
+    print("RMSE multiple linear regression: " + str(multipleLinearRegression[1]))
 
+
+runAllModelsTask2(True)
 
 
 
 # ---------------- For task 3 ----------------
-
 def createTimeserie(powers, windowsize):
     rows = len(powers) - windowsize
     x = np.zeros((rows, windowsize))
@@ -194,85 +221,70 @@ def createTimeserie(powers, windowsize):
     return x, y
 
 
-def split_sequence(sequence, n_steps):
-    X, y = list(), list()
-    for i in range(len(sequence)):
-        end_ix = i + n_steps
-        if end_ix > len(sequence)-1:
-            break
-
-        seq_x, seq_y = sequence[i:end_ix], sequence[end_ix]
-        X.append(seq_x)
-        y.append(seq_y)
-
-    return X, y
-
-
-def timeserieLinReg(x, y, actualPower, forecastInput):
+def timeserieLinReg(x, y, forecastInput, actualPower, windowsize, showPlots):
     model = LinearRegression()
     model.fit(x, y)
+    predictedPower = model.predict(forecastInput)
 
-    predictedPower = model.predict(actualPower[:-1].reshape(-1, 1))
-    powerSolution = actualPower[1:]
+    actualPowerComparison = actualPower[windowsize:]
 
-    print("RMSE timeseries linear regression ", RMSE(powerSolution, predictedPower))
+    rmse = RMSE(actualPowerComparison, predictedPower)
 
-    plotResults(actualPower, predictedPower, "timeseries linear regression")
+    if showPlots:
+        plotResults(actualPower[windowsize:], predictedPower, "timeseries linear regression")
 
-    plt.scatter(actualPower[:-1], powerSolution)
-    plt.plot(actualPower[:-1], predictedPower)
-    plt.show()
-
-    # predicted = model.predict(powers[-1].reshape(1,-1))
-    # nextPredicted = model.predict(predicted.reshape(1,-1))
+    return predictedPower, rmse
 
 
-def timeserieSupportedVectorRegression(x, y, actualPower):
+def timeserieSupportedVectorRegression(x, y, forecastInput, actualPower, windowsize, showPlots):
     model = SVR()
     model.fit(x, y)
-    predictedPower = model.predict(actualPower[:-1].reshape(-1, 1))
 
-    powerSolution = actualPower[1:]
+    predictedPower = model.predict(forecastInput)
+    actualPowerComparison = actualPower[windowsize:]
 
-    print("RMSE timeserie SVR", RMSE(powerSolution, predictedPower))
+    rmse = RMSE(actualPowerComparison, predictedPower)
 
-    plotResults(actualPower, predictedPower, "timeserie SVR")
+    if showPlots:
+        plotResults(actualPower[windowsize:], predictedPower, "timeseries SVR")
+
+    return predictedPower, rmse
 
 
-def timeserieArtificialNeuralNetworks(x, y, actualPower):
-    inputs = Input(shape=(1,))
-
+def timeserieArtificialNeuralNetworks(x, y, forecastInput, actualPower, windowsize, showPlots):
     # Layers and number of neurons in hidden layers
+    inputs = Input(shape=(windowsize,))
     layer = Dense(32, activation='relu')(inputs)
     layer = Dense(64, activation='relu')(layer)
     layer = Dense(100, activation='relu')(layer)
     predictions = Dense(1)(layer)
 
-    # The model that includes the input layer and three Dense layers
     model = Model(inputs=inputs, outputs=predictions)
     model.compile(optimizer='rmsprop',
                   loss='mse',
                   metrics=['mse'])
 
-    model.fit(x, y, epochs=500, batch_size=100, verbose=1, validation_split=0.2)  # starts training
+    model.fit(x, y, epochs=100, batch_size=100, verbose=1, validation_split=0.2)
 
-    # Predicting power with new data
-    predictedPower = model.predict(actualPower[:-1].reshape(-1, 1))
+    predictedPower = model.predict(forecastInput)
+    actualPowerComparison = actualPower[windowsize:].reshape((-1, 1))
 
-    actualPower = actualPower.reshape((-1, 1))
+    rmse = RMSE(actualPowerComparison, predictedPower)
 
-    print("RMSE Neural networks", RMSE(actualPower[0:len(actualPower)-1], predictedPower))
+    if showPlots:
+        plotResults(actualPower[windowsize:], predictedPower, "timeserie ANN")
 
-    plotResults(actualPower[0:len(actualPower)-1], predictedPower, "timeserie ANN")
+    return predictedPower, rmse
 
-def timeserieRecurrantNeuralNetwork(x, y, actualPower):
-    # reshaping x input
-    x = np.reshape(x, (x.shape[0], 1, x.shape[1]))
-    actualPower = np.reshape(actualPower, (len(actualPower), 1, 1))
+
+def timeserieRecurrantNeuralNetwork(x, y, forecastInput, actualPower, windowsize, showPlots):
+    # reshaping input to (batch_size, timesteps, input_dim)
+    x = x.reshape(-1, 1, windowsize)
+    forecastInput3D = np.reshape(forecastInput, (len(forecastInput), 1, windowsize))
 
     # using LSTM layer, used to accept a sequence of input data and make prediction
     model = Sequential()
-    model.add(LSTM(32, input_shape=(1, 1), activation='relu', return_sequences=True))
+    model.add(LSTM(32, input_shape=(1, windowsize), activation='relu', return_sequences=True))
     model.add(Dropout(0.1))
     model.add(LSTM(64, activation='relu'))
     model.add(Dropout(0.1))
@@ -280,43 +292,45 @@ def timeserieRecurrantNeuralNetwork(x, y, actualPower):
     model.add(Dropout(0.2))
     model.add(Dense(1))
 
-    # model.add(LSTM(128, input_shape=(1,1), activation='relu', return_sequences=True)) #input_shape=(n_steps,1)
-    # model.add(Dropout(0.2))
-    # model.add(LSTM(128, activation='relu'))
-    # model.add(Dropout(0.1))
-    # model.add(Dense(32, activation='relu'))
-    # model.add(Dropout(0.2))
-    # model.add(Dense(10, activation='relu'))
-
     model.compile(
         loss='mse',
-        optimizer='adam',
+        optimizer='rmsprop',
         metrics=['mse'],
     )
-    model.summary()
 
-    model.fit(x, y, epochs=50, batch_size=100, verbose=1)
-    predictedPower = model.predict(actualPower)
-    print("RMSE RNN", RMSE(actualPower[0:len(actualPower) - 1], predictedPower))
+    model.fit(x, y, epochs=100, batch_size=100, verbose=1)
+    predictedPower = model.predict(forecastInput3D)
+    actualPowerComparison = actualPower[windowsize:].reshape((-1, 1))
+
+    rmse = RMSE(actualPowerComparison, predictedPower)
+
+    if showPlots:
+        plotResults(actualPower[windowsize:], predictedPower, "timeseries RNN")
+
+    return predictedPower, rmse
+
+def runAllTimeseriesModelsWindowsize(actualPower, windowsize, showPlots):
+    timeseries = createTimeserie(power_training, windowsize)
+    x = timeseries[0]
+    y = timeseries[1]
+    forecastInput = createTimeserie(actualPower, windowsize)
+
+    tsLinReg = timeserieLinReg(x, y, forecastInput[0], actualPower, windowsize, plotResults)
+    tsSVR = timeserieSupportedVectorRegression(x, y, forecastInput[0], actualPower, windowsize, plotResults)
+    tsANN = timeserieArtificialNeuralNetworks(x, y, forecastInput[0], actualPower, windowsize, plotResults)
+    tsRNN = timeserieRecurrantNeuralNetwork(x, y, forecastInput[0], actualPower, windowsize, plotResults)
+
+    if showPlots:
+        plotResults3Inputs(actualPower, tsLinReg[0], tsSVR[0], "actual power", "linear regression", "supported vector regression",
+                            "actual power, linear regression and SVR comparison for timeseries")
+
+        plotResults3Inputs(actualPower, tsANN[0], tsRNN[0], "actual power", "artificial neural network", "recurrant neural network",
+                            "actual power, ANN and RNN comparison for timeseries")
+
+    print("RMSE timeseries LinReg:" + str(tsLinReg[1]) + ", windowsize = " + str(windowsize))
+    print("RMSE timeseries SVR:" + str(tsSVR[1]) + ", windowsize = " + str(windowsize))
+    print("RMSE timeseries ANN:" + str(tsANN[1]) + ", windowsize = " + str(windowsize))
+    print("RMSE timeseries RNN:" + str(tsRNN[1]) + ", windowsize = " + str(windowsize))
 
 
-
-
-timeserie = createTimeserie(power, 1)
-x = timeserie[0]
-y = timeserie[1]
-
-
-
-
-# print(x.shape, "x shape")
-# timeserieLinReg(x, y, actualPower, ws)
-# timeserieSupportedVectorRegression(x, y, actualPower)
-# timeserieArtificialNeuralNetworks(x, y, actualPower)
-timeserieRecurrantNeuralNetwork(x, y, actualPower)
-
-
-# a = [1,2,3,4,5,6,7,8,9] [[1,2], [2,3], ]
-# res = split_sequence(a, 3)
-# print(res[0])
-# print(res[1])
+# runAllTimeseriesModelsWindowsize(actualPower, 1, True)
